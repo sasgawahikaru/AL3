a092779e0b9c42f59b5952bd58c7273c1f13f92b
sasagawahikaru <k021g1383@g.neec.ac.jp>
2022-05-11 13:05:58 +0900

    02_04　終わり

--- scene/GameScene.cpp
+++ scene/GameScene.cpp
@@ -1,66 +1,188 @@
 ﻿#include "GameScene.h"
 #include "TextureManager.h"
 #include <cassert>
-
+#include<time.h>
 using namespace DirectX;
 
-//コンストラクタ
 GameScene::GameScene() {}
 
-//デストラクタ
 GameScene::~GameScene() {
-	//delete sprite_;
-	//delete model_;
+	delete spriteBG_;
+	delete modelStage_;
+	delete modelPlayer_;
+	delete modelBeam_;
+	delete modelEnemy_;
 }
 
 void GameScene::Initialize() {
 
+	srand((unsigned int)time(NULL));
 	dxCommon_ = DirectXCommon::GetInstance();
 	input_ = Input::GetInstance();
 	audio_ = Audio::GetInstance();
 	debugText_ = DebugText::GetInstance();
 
-	textureHandle_ = TextureManager::Load("mario.jpg");
-	soundDataHandle_ = audio_->LoadWave("se_sad03.wav");
-	//voiceHandle_ = audio_->PlayWave(soundDataHandle_, true);
+	// BG(2D)
+	textureHandleBG_ = TextureManager::Load("bg.jpg");
+	spriteBG_ = Sprite::Create(textureHandleBG_, {0, 0});
 
-	sprite_ = Sprite::Create(textureHandle_, {100, 50});
-	//audio_->PlayWave(soundDataHandle_);
-
-	model_ = Model::Create();
-
-	worldTransform_.Initialize();
+	//ビュープロジェクション初期化
+	viewProjection_.eye = {0, 1, -6};
+	viewProjection_.target = {0, 1, 0};
 	viewProjection_.Initialize();
 
-	worldTransform_.scale_ = {5.0f, 5.0f, 5.0f};
-	worldTransform_.Initialize();
+	//ステージ
+	textureHandleStage_ = TextureManager::Load("stage.jpg");
+	modelStage_ = Model::Create();
+	worldTransformStage_.translation_ = {0, -1.5f, 0};
+	worldTransformStage_.scale_ = {4.5f, 1, 40};
+	worldTransformStage_.Initialize();
 
-	worldTransform_.rotation_ = {XM_PI / 4.0f, XM_PI / 4.0f, 0.0f};
-	worldTransform_.Initialize();
+	//プレイヤー
+	textureHandlePlayer_ = TextureManager::Load("player.png");
+	modelPlayer_ = Model::Create();
+	worldTransformPlayer_.scale_ = {0.5f, 0.5f, 0.5f};
+	worldTransformPlayer_.Initialize();
 
-	worldTransform_.translation_ = {10.0f, 10.0f, 10.0f};
-	worldTransform_.Initialize();
+	//ビーム
+	textureHandleBeam_ = TextureManager::Load("beam.png");
+	worldTransformBeam_.scale_ = {0.3f, 0.3f, 0.3f};
+	worldTransformBeam_.Initialize();
+	modelBeam_ = Model::Create();
 
+	//敵
+	textureHandleEnemy_ = TextureManager::Load("enemy.png");
+	modelEnemy_ = Model::Create();
+	worldTransformEnemy_.scale_ = {0.5f, 0.5f, 0.5f};
+	worldTransformEnemy_.Initialize();
 }
-//更新
+
 void GameScene::Update() {
-	if (input_->TriggerKey(DIK_SPACE))
-		//音声停止
-		audio_->StopWave(voiceHandle_);
-
-	//	debugText_->Print("ikisugii 19 19", 50, 50, 1.0f);
-	//	debugText_->SetPos(50, 70);
-	//	debugText_->Printf("year:%d", 2001);
-
-	value_++;
-
-	std::string strDebug = std::string("value") + std::to_string(value_);
-
-	debugText_->Print(strDebug, 50, 50, 1.0f);
+	PlayerUpdate();
+	BeamUpdate();
+	EnemyUpdate();
+	CollisionUpdate();
 }
+//プレイヤー
+void GameScene::PlayerUpdate() {
+	//移動
+	//右
+	if (input_->PushKey(DIK_RIGHT)) {
+		worldTransformPlayer_.translation_.x += 0.1f;
+		if (worldTransformPlayer_.translation_.x > 4) {
+			worldTransformPlayer_.translation_.x = 4;
+		}
+	}
+	//左
+	if (input_->PushKey(DIK_LEFT)) {
+		worldTransformPlayer_.translation_.x -= 0.1f;
+		if (worldTransformPlayer_.translation_.x < -4) {
+			worldTransformPlayer_.translation_.x = -4;
+		}
+	}
+	//行列更新
+	worldTransformPlayer_.UpdateMatrix();
+}
+
+//ビーム更新
+void GameScene::BeamUpdate() {
+	//移動
+	BeamMove();
+	//
+	BeamBorn();
+	//行列更新
+	worldTransformBeam_.UpdateMatrix();
+}
+
+//ビーム移動
+void GameScene::BeamMove() {
+	if (beamflag_ == 1) {
+		worldTransformBeam_.translation_.z += 0.3f;
+		worldTransformBeam_.rotation_.x += 0.3f;
+		if (worldTransformBeam_.translation_.z > 40) {
+			beamflag_ = 0;
+		}
+	}
+}
+void GameScene::BeamBorn() {
+	if (beamflag_ == 0) {
+		if (input_->PushKey(DIK_SPACE)) {
+			beamflag_ = 1;
+			worldTransformBeam_.translation_.x = worldTransformPlayer_.translation_.x;
+			worldTransformBeam_.translation_.z = worldTransformPlayer_.translation_.z;
+		}
+	}
+}
+
+//敵更新
+void GameScene::EnemyUpdate() {
+	//移動
+	EnemyMove();
+
+	enemyBorn();
+	//行列更新
+	worldTransformEnemy_.UpdateMatrix();
+}
+
+//敵移動
+void GameScene::EnemyMove() {
+	if (enemyFlag_ == 1) {
+		worldTransformEnemy_.translation_.z -= 0.7f;
+		worldTransformEnemy_.rotation_.x -= 0.1f;
+	}
+	if (worldTransformEnemy_.translation_.z < -5) {
+		enemyFlag_ = 0;
+	}
+	if (enemyFlag_ == 0) {
+		worldTransformEnemy_.translation_.z = 40.0f;
+		enemyFlag_ = 1;
+	}
+}
+void GameScene::enemyBorn() {
+	if (enemyFlag_ == 0) {
+		enemyFlag_ = 1;
+		worldTransformEnemy_.translation_.z =40.0f;
+		//	if (worldTransformEnemy_.translation_.z < -5) {
+		int x = rand() % 80;
+		float x2 = (float)x / 10 - 4;
+		worldTransformEnemy_.translation_.x = x2;
+//		worldTransformEnemy_.translation_.z = 40.0f;
+	}
+}
+void GameScene::CollisionUpdate() {
+	Collision();
+	CollisionPlayerEnemy();
+}
+
+//衝突判定
+void GameScene::Collision() {
+	CollisionPlayerEnemy();
+//	CollisionBeamEnemy();
+}
+void GameScene::CollisionPlayerEnemy() {
+	if (enemyFlag_ == 1) {
+		float dx = abs(worldTransformPlayer_.translation_.x - worldTransformEnemy_.translation_.x);
+		float dz = abs(worldTransformPlayer_.translation_.z - worldTransformEnemy_.translation_.z);
+		//衝突したら
+		if (dx < 1 && dz < 1) {
+
+			enemyFlag_ = 0;
+		}
+	}
+}
+//void GameScene::CollisionBeamEnemy() {
+//	if (enemyFlag_ == 1) {
+//		float dx = abs(worldTransformBeam_.translation_.x - worldTransformEnemy_.translation_.x);
+//		float dz = abs(worldTransformBeam_.translation_.z - worldTransformEnemy_.translation_.z);
+		//衝突したら
+//		if (dx < 1 && dz < 1) {
+
+//			enemyFlag_ = 0;
+//		}
+//	}
+//}
 
 void GameScene::Draw() {
-
 	// コマンドリストの取得
 	ID3D12GraphicsCommandList* commandList = dxCommon_->GetCommandList();
 
@@ -71,7 +193,7 @@ void GameScene::Draw() {
 	/// <summary>
 	/// ここに背景スプライトの描画処理を追加できる
 	/// </summary>
-
+	spriteBG_->Draw();
 	// スプライト描画後処理
 	Sprite::PostDraw();
 	// 深度バッファクリア
@@ -85,11 +207,21 @@ void GameScene::Draw() {
 	/// <summary>
 	/// ここに3Dオブジェクトの描画処理を追加できる
 	/// </summary>
+	modelStage_->Draw(worldTransformStage_, viewProjection_, textureHandleStage_);
 
-	model_->Draw(worldTransform_, viewProjection_, textureHandle_);
+	modelPlayer_->Draw(worldTransformPlayer_, viewProjection_, textureHandlePlayer_);
 
+	if (beamflag_ == 1) {
+		modelBeam_->Draw(worldTransformBeam_, viewProjection_, textureHandleBeam_);
+	}
+
+	modelEnemy_->Draw(worldTransformEnemy_, viewProjection_, textureHandleEnemy_);
 	// 3Dオブジェクト描画後処理
 	Model::PostDraw();
+	//スコア
+	char str[100];
+	sprintf_s(str, "SCORE %d", gameScore_);
+	debugText_->Print(str, 200, 10, 2);
 #pragma endregion
 
 #pragma region 前景スプライト描画
@@ -99,20 +231,12 @@ void GameScene::Draw() {
 	/// <summary>
 	/// ここに前景スプライトの描画処理を追加できる
 	/// </summary>
-	//sprite_->Draw();
 
 	// デバッグテキストの描画
 	debugText_->DrawAll(commandList);
 	//
 	// スプライト描画後処理
 	Sprite::PostDraw();
-	//スプライトの今の座標を取得
-//	XMFLOAT2 position = sprite_->GetPosition();
-	//座標を｛2,0｝移動
-//	position.x += 2.0f;
-//	position.y += 1.0f;
-	//移動した座標をスプライトに反映
-//	sprite_->SetPosition(position);
 
 #pragma endregion
 }
--- scene/GameScene.h
+++ scene/GameScene.h
@@ -15,6 +15,7 @@
 /// ゲームシーン
 /// </summary>
 class GameScene {
+
   public: // メンバ関数
 	/// <summary>
 	/// コンストクラタ
@@ -41,6 +42,34 @@ class GameScene {
 	/// </summary>
 	void Draw();
 
+	//プレイヤー更新
+	void PlayerUpdate();
+
+	//ビーム更新
+	void BeamUpdate();
+
+	//ビーム移動
+	void BeamMove();
+
+	//ビーム発生
+	void BeamBorn();
+
+	//ビーム存在フラグ
+	int beamflag_ = 0;
+
+	//敵生存フラグ
+	int enemyFlag_ = 0;
+	void enemyBorn();
+
+	//衝突判定
+	void Collision();
+	void CollisionPlayerEnemy();
+	//衝突　ビームと敵
+//	void CollisionBeamEnemy();
+
+	//スコア
+	int gameScore_ = 0;
+
   private: // メンバ変数
 	DirectXCommon* dxCommon_ = nullptr;
 	Input* input_ = nullptr;
@@ -50,16 +79,33 @@ class GameScene {
 	/// <summary>
 	/// ゲームシーン用
 	/// </summary>
-	uint32_t textureHandle_ = 0;
-	uint32_t soundDataHandle_ = 0;
-	uint32_t voiceHandle_ = 0;
 
-	int32_t value_ = 0;
-
-	Sprite* sprite_ = nullptr;
-
-	Model* model_ = nullptr;
-
-	WorldTransform worldTransform_;
+	// BG
+	uint32_t textureHandleBG_ = 0;
+	Sprite* spriteBG_ = nullptr;
+	//ビュープロジェクション
 	ViewProjection viewProjection_;
+	//ステージ
+	uint32_t textureHandleStage_ = 0;
+	Model* modelStage_ = nullptr;
+	WorldTransform worldTransformStage_;
+
+	//プレイヤー
+	uint32_t textureHandlePlayer_ = 0;
+	Model* modelPlayer_ = nullptr;
+	WorldTransform worldTransformPlayer_;
+
+	//ビーム
+	uint32_t textureHandleBeam_ = 0;
+	Model* modelBeam_ = nullptr;
+	WorldTransform worldTransformBeam_;
+
+	//敵
+	uint32_t textureHandleEnemy_ = 0;
+	Model* modelEnemy_ = nullptr;
+	WorldTransform worldTransformEnemy_;
+	//
+	void EnemyUpdate();
+	void EnemyMove();
+	void CollisionUpdate();
 };
